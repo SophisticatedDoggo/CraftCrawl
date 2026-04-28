eb7d1022db6b639e82ab7b37a0b1c75140b0f977
Noah Lawrence <161643018+SophisticatedDoggo@users.noreply.github.com>
2026-04-28 09:45:33 -0400

    Minor updates to index.php and schema

--- schema.sql
+++ schema.sql
@@ -1,17 +1,16 @@
-CREATE DATABASE craft_crawl IF NOT EXISTS;
+CREATE DATABASE IF NOT EXISTS craft_crawl;
 USE craft_crawl;
 
-CREATE TABLE users IF NOT EXISTS (
+CREATE TABLE IF NOT EXISTS users (
     id INT AUTO_INCREMENT PRIMARY KEY,
     fName VARCHAR(50) NOT NULL,
     lName VARCHAR(50) NOT NULL,
-    email VARCHAR(255) NOT NULL
-    createdAt DATE NOT NULL
+    email VARCHAR(255) NOT NULL,
+    password_hash VARCHAR(255) NOT NULL,
+    createdAt DATETIME NOT NULL
 );
 
-
-
-CREATE TABLE businesses IF NOT EXISTS (
+CREATE TABLE IF NOT EXISTS businesses (
     id INT AUTO_INCREMENT PRIMARY KEY,
     bname VARCHAR (255) NOT NULL,
     bEmail VARCHAR(255),
@@ -20,15 +19,14 @@ CREATE TABLE businesses IF NOT EXISTS (
     bWebsite VARCHAR(2048)
 );
 
-CREATE TABLE reviews IF NOT EXISTS (
+CREATE TABLE IF NOT EXISTS reviews (
     id INT AUTO_INCREMENT PRIMARY KEY,
     rating INT NOT NULL,
     user_id INT NOT NULL,
     business_id INT NOT NULL,
-    notes VARCHAR(n),
+    notes VARCHAR(2048),
     CONSTRAINT fk_userId FOREIGN KEY (user_id)
     REFERENCES users(id),
     CONSTRAINT fk_businessId FOREIGN KEY (business_id)
     REFERENCES businesses(id)
-
 );
