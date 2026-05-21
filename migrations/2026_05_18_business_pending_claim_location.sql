a17e549bc2591308dc476aab37524434b88f0fb1
Noah Lawrence <161643018+SophisticatedDoggo@users.noreply.github.com>
2026-05-21 12:54:07 -0400

    Removing migration files from May 15-18

--- migrations/2026_05_18_business_pending_claim_location.sql
+++
@@ -1,6 +0,0 @@
-
-
-ALTER TABLE business_accounts
-    ADD COLUMN pending_claim_location_id INT NULL AFTER rejectionReason,
-    ADD CONSTRAINT fk_business_account_pendingClaimLocationId
-        FOREIGN KEY (pending_claim_location_id) REFERENCES locations(id);
